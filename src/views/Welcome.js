781e13d078c6c8d975f3e3ab364e80a04a8c45d6
johnsyntaxe <johnsyntaxes@gmail.com>
2021-08-10 16:55:29 +0100

    Added Error 500 forgotten

---
+++ src/views/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div className="my-5 d-flex align-items-center justify-content-center">
+            <h1>Bienvenue sur Chat & Yamo Panel d'aministration</h1>
+        </div>
+    );
+};
+
+export default Welcome;
