ac547b305af13cf93f72cb9327deb71de9949715
Croquignolex <gpetitalex10@gmail.com>
2022-05-21 11:40:34 +0100

    Improve bacloffice user id for delete option

--- src/models/BackofficeUser.js
+++ src/models/BackofficeUser.js
@@ -2,7 +2,7 @@ export default class BackofficeUser {
     constructor(backofficeUser) {
         Object.assign(this, backofficeUser);
 
-        this.id = this.entityId;
+        this.id = this.userId || this.entityId;
     }
 
     get getRoles() {
